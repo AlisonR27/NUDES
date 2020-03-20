d00b81fcc21e478af6755a264d904029bba67631
Alison Ranier <alisonranier@gmail.com>
2020-03-19 21:10:15 -0300

    Edição na página indice, para teste de dependências e do funcionamento do código PHP. Agora página exibe logo e titulo.

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-Hello World!
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<div class="w-100 h-100 bg-dark">
+	<div class="row m-0">
+		<div class="col-4 offset-4">
+			<img src="<?php echo get_bloginfo('template_url');?>/assets/imgs/logo.png"/>
+		</div>
+	</div>
+	<div class="row m-0">
+	  	<div class="col-4 offset-4">
+			<h1 class="h1 text-center text-light">NUDES</h1>
+		</div>
+	</div>
+</div>
