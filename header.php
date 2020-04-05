5119ceaacd2b06c8170f69df10eefae568dda035
Alison Ranier <alisonranier@gmail.com>
2020-04-05 05:44:16 -0300

    Adição das imagens dos cards da página índice, implementação da navbar e dos primeiros elementos da página índice.

--- header.php
+++ header.php
@@ -20,18 +20,27 @@ $blog_url = get_bloginfo('url');
             endif; 
         ?>
     </title>
+    <!-- Define ícone da página no navegador -->
+    <link rel="icon" href="<?php echo $assets?>/imgs/favicon.ico">
+    <!-- Fonte  -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300&display=swap" rel="stylesheet">
     <!-- JQuery -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <!-- Bootstrap -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <link href="<?php echo $assets?>/css/style.css" rel="stylesheet" type="text/css">
+    <?php 
+            if (is_front_page() && is_home()):?>
+    <link href="<?php echo $assets?>/css/index.css" rel="stylesheet" type="text/css">
+    <?php endif;?>
+
 </head>
 <body>
     <header class="header">
-        <nav class="navbar navbar-expand-md justify-content-between px-5 py-2">
-            <a class="navbar-brand" href="<?php echo $blog_url?>">
-                <img src="<?php echo $assets?>/imgs/logo.png" alt="" class="img-fluid" id="navbar-logo" style="max-height:50px;">
+        <nav class="navbar fixed-top navbar-expand-md justify-content-between  px-5 py-2">
+            <a class="navbar-brand d-flex align-items-center" href="<?php echo $blog_url?>">
+                <img src="<?php echo $assets?>/imgs/flat-logo.png" alt="" class="img-fluid" id="navbar-logo">
                 NUDES
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -65,7 +74,9 @@ $blog_url = get_bloginfo('url');
                         </a>
                     </li>
                 </ul>
-                <a href="" class="btn btn-light rounded-pill px-5 py-2 ml-3">Contato</a>
+                <a href="" class="btn btn-light text-dark rounded-pill px-5 py-2 ml-3">Contato</a>
             </div>
         </nav>
     </header>
+    <div id="main">
+    
--- index.php
+++ index.php
@@ -1,14 +1,55 @@
-<?php get_header();?>
-<div class="w-100 h-100 bg-dark">
-	<div class="row m-0">
-		<div class="col-4 offset-4">
-			<img src="<?php echo get_bloginfo('template_url');?>/assets/imgs/logo.png"/>
+<?php get_header();
+$assets = get_bloginfo('template_url').'/assets';
+
+?>
+	<section id="Apresentacao">
+		<div class="show-banner mt-n5">
+			<div class="show-banner-content">
+				<h1>Núcleo de Desenvolvimento de Software do IFRN</h1>
+			</div>
+			<img class="bg-img" src="<?php echo $assets?>/imgs/ifrn.png" />
+
 		</div>
-	</div>
-	<div class="row m-0">
-	  	<div class="col-4 offset-4">
-			<h1 class="h1 text-center text-light">NUDES</h1>
+	</section>
+	<section id="pesquisa" class="pesquisa py-5">
+		<div class="container mt-1">
+			<h2 class="text-center mb-5">Áreas de Pesquisa</h2>
+			<div class="row justify-content-between">
+				<div class="card col-3 mx-2 py-4 px-3 mb-5 text-center">
+					<img src="<?php echo $assets?>/index/card1.png" alt="">
+					<span class="mt-3" >Temas de convergência: Cidades Inteligentes</span>
+				</div>
+				<div class="card col-3 mx-2 py-4 px-3 mb-5 text-center">
+					<img src="<?php echo $assets?>/index/card2.png" alt="">
+					<span class="mt-3" >Qualidade e Produtividade de Software </span>
+				</div>
+				<div class="card col-3 mx-2 py-4 px-3 mb-5 text-center">
+					<img src="<?php echo $assets?>/index/card3.png" alt="">
+					<span class="mt-3" >Banco de Dados, Big Data e Análise de Dados</span>
+				</div>
+				<div class="card col-3 mx-2 py-4 px-3 mb-5 text-center">
+					<img src="<?php echo $assets?>/index/card4.png" alt="">
+					<span class="mt-3" >Sitemas Embarcados e Móveis</span>
+				</div>
+				<div class="card col-3 mx-2 py-4 px-3 mb-5 text-center">
+					<img src="<?php echo $assets?>/index/card5.png" alt="">
+					<span class="mt-3" >Desenvolvimento de Jogos Digitais</span>
+				</div>
+				<div class="card col-3 mx-2 py-4 px-3 mb-5 text-center">
+					<img src="<?php echo $assets?>/index/card6.png" alt="">
+					<span class="mt-3" >Engenharia de Serviços</span>
+				</div>
+			</div>
 		</div>
+	</section>
+	<section id="categorias" class="categorias py-5">
+		<div class="container">
+			<div class="row">
+				<div>
+			</div>
+		</div>
+	</section>
+	<div style="height:500px;">
+
 	</div>
-</div>
 <?php get_footer();?>
